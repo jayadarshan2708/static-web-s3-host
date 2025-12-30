b1b678a4cc9900dbb7a2c9e9831515f471cdbe62
jaya_darshan <santadarshan07@gmail.com>
2025-12-30 11:22:30 +0530

    edit in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,33 +1,44 @@
 provider "aws" {
-	region = "us-east-1"
+  region = "us-east-1"
 }
-resource = "aws_s3_bucket_website_configuration" "website" {
-	bucket = "devops-static-site"
+
+resource "aws_s3_bucket" "static_site" {
+  bucket = "devops-static-site"
+
+  tags = {
+    Name = "StaticWebsite"
+  }
 }
 
 resource "aws_s3_bucket_website_configuration" "website" {
-	bucket = aws_s3_bucket.static_site.id
-	index_document {
-		suffix = "index.html"
-	}
+  bucket = aws_s3_bucket.static_site.id
+
+  index_document {
+    suffix = "index.html"
+  }
 }
 
-resource "aws_s3_bucket_public_access_block "public" {
-	bucket = aws_s3_bucket.static.site.id
-	block_public_acls = false
-	block_public_policy = false
+resource "aws_s3_bucket_public_access_block" "public" {
+  bucket = aws_s3_bucket.static_site.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
-resource  "aws_s3_bucket_public_policy' "policy" {
-	bucket = aws_s3_bucket.static_site.id
+resource "aws_s3_bucket_policy" "policy" {
+  bucket = aws_s3_bucket.static_site.id
 
-	policy = jsonencode({
-		Version = "2012-10-17"
-		Statement = [{
-			Effect = "Allow"
-			Principal = "*"
-			Action = "s3.GetObject"
-			Resource = "${aws_s3_bucket.static_site.arn}/*"
-		}]
-	})
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = "*"
+        Action = "s3:GetObject"
+        Resource = "${aws_s3_bucket.static_site.arn}/*"
+      }
+    ]
+  })
 }
